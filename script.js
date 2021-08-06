9e6ab1082ef08ef71906f8c5175592bc9ac37e5b
WestKostikov <maksrey@mail.ru>
2021-08-06 15:28:47 +0300

    Add function rememberMyFilms
    Add function rememberMyFilms and cut cycle into the function

--- script.js
+++ script.js
@@ -20,18 +20,22 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
+
+rememberMyFilms();
 
 if (personalMovieDB.count < 10) {
     alert('Просмотрено довольно мало фильмов');
